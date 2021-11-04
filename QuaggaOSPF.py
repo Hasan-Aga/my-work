647849541288ae709a9dc28834f4d890fc0bbfa6
Hasan-Alsulaiman <careerforma@gmail.com>
2021-11-04 16:13:31 +0300

    create router

---
+++ QuaggaOSPF.py
@@ -0,0 +1,19 @@
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import Node
+from mininet.log import setLogLevel, info
+from mininet.cli import CLI
+import time
+import os
+
+class LinuxRouter( Node ):
+    "A Node with IP forwarding enabled."
+
+    def config( self, **params ):
+        super( LinuxRouter, self).config( **params )
+        # Enable forwarding on the router
+        self.cmd( 'sysctl net.ipv4.ip_forward=1' )
+
+    def terminate( self ):
+        self.cmd( 'sysctl net.ipv4.ip_forward=0' )
+        super( LinuxRouter, self ).terminate()
