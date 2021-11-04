24528e1d4e646376240e7e1b5c66d7eaaf3c1ebd
Hasan-Alsulaiman <careerforma@gmail.com>
2021-11-04 19:43:50 +0300

    create installation script

---
+++ install_quagga_2.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+cd /home
+sudo git clone https://github.com/Quagga/quagga.git
+
+cd quagga/
+sudo apt update
+
+sudo apt install gawk
+
+sudo apt install libreadline6 libreadline6-dev
+
+sudo apt install libtool
+
+sudo libtoolize --force
+
+sudo aclocal
+
+sudo autoheader
+
+sudo automake --force-missing --add-missing
+
+sudo autoconf
+
+sudo apt install libc-ares-dev
+
+sudo ./configure  --enable-vtysh --enable-user=root --enable-group=root --enable-vty-group=root
+
+sudo make install
+
+spd-say done
