25198558487bb2fd00dfc04e58d89cc318148406
Hasan-Alsulaiman <careerforma@gmail.com>
2021-11-04 16:18:18 +0300

    create topology

--- QuaggaOSPF.py
+++ QuaggaOSPF.py
@@ -17,3 +17,19 @@ class LinuxRouter( Node ):
     def terminate( self ):
         self.cmd( 'sysctl net.ipv4.ip_forward=0' )
         super( LinuxRouter, self ).terminate()
+
+class NetworkTopo( Topo ):
+    "A LinuxRouter connecting three IP subnets"
+
+    def build( self, **_opts):
+        defaultIP1 = '10.0.3.10/24'  # IP address for r0-eth1
+        defaultIP2 = '10.0.3.20/24' 
+        router1 = self.addNode( 'r1', cls=LinuxRouter, ip=defaultIP1 )
+	    router2 = self.addNode( 'r2', cls=LinuxRouter, ip=defaultIP2 )
+
+        h1 = self.addHost( 'h1', ip='10.0.1.100/24', defaultRoute='via 10.0.1.10') #define gateway
+        h2 = self.addHost( 'h2', ip='10.0.2.100/24', defaultRoute='via 10.0.2.20')
+
+        self.addLink(router1,router2,intfName1='r1-eth1',intfName2='r2-eth1')
+	    self.addLink(h1,router1,intfName2='r1-eth2',params2={ 'ip' : '10.0.1.10/24' })#params2 define the eth2 ip address
+	    self.addLink(h2,router2,intfName2='r2-eth2',params2={ 'ip' : '10.0.2.20/24' })
