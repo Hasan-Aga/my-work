fe474beb62c8121fb79425ccd59f862cf7905d70
Hasan-Alsulaiman <careerforma@gmail.com>
2021-11-28 19:31:34 +0300

    comment out old code

--- topologies/simple_topology/two_routers_1_0.py
+++ topologies/simple_topology/two_routers_1_0.py
@@ -36,7 +36,7 @@ class NetworkTopo( Topo ):
         h1 = self.addHost( 'h1', ip='10.0.1.100/24', defaultRoute='via 10.0.1.10') #define gateway
         h2 = self.addHost( 'h2', ip='10.0.2.100/24', defaultRoute='via 10.0.2.20')
 
-        self.addLink(routers["r1"],routers["r2"],intfName1='r1-eth1',intfName2='r2-eth1')
+        # self.addLink(routers["r1"],routers["r2"],intfName1='r1-eth1',intfName2='r2-eth1')
 
 
         self.addLink(h1,routers["r1"],intfName2='r1-eth2',params2={ 'ip' : '10.0.1.10/24' })#params2 define the eth2 ip address
