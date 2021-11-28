b7e8196bc0a62f0f23ca3c7a50dfc7086193a87f
Hasan-Alsulaiman <careerforma@gmail.com>
2021-11-28 16:00:44 +0300

    improve json

--- topologies/simple_topology/jsonTest.py
+++ topologies/simple_topology/jsonTest.py
@@ -4,11 +4,8 @@ import os
 
 def file_path(relative_path):
     dir = os.path.dirname(os.path.abspath(__file__))
-    print(dir)
     split_path = relative_path.split("/")
-    print(split_path)
     new_path = os.path.join(dir, *split_path)
-    print(new_path)
     return new_path
 
 
@@ -16,7 +13,7 @@ with open(file_path("/addressConfiguration.json"), "r") as addressFile:
             data = json.load(addressFile)
 
 routers = {}
-for index,router in enumerate(data):
-    routers["router" + str(index+1)] = data[router]["interfaces"]["real"]["defaultIP"]
+for index,router in enumerate(data["routers"]):
+    routers["router" + str(index+1)] = data["routers"][router]["interfaces"]["real"]["defaultIP"]
 
 print(routers)
