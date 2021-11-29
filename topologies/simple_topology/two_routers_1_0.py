a0e411f25856d82f3045776083ae7a33d39c1128
Hasan-Alsulaiman <careerforma@gmail.com>
2021-11-29 20:00:52 +0300

    relocate config_templates

--- topologies/simple_topology/two_routers_1_0.py
+++ topologies/simple_topology/two_routers_1_0.py
@@ -95,9 +95,9 @@ def loadOspfForAllRouters(net:Mininet, data:dict):
         device=net.getNodeByName(r)
         device.cmd(f'ospfd -f /usr/local/etc/{r}ospfd.conf -d -z ~/{r}zebra.api -i ~/{r}ospfd.interface')
 
-def generateZebraConfFIles(net:Mininet, data:dict):
+def generateZebraConfFIles(data:dict):
     routers = getRouterNames(data)
-    for r in routers:
+    # for r in routers:
 
 def run():
     "Test linux router"
