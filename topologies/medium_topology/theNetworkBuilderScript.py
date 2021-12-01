41a87eb5b8ffa9fd8e79e7a725dee74cded91174
Hasan-Alsulaiman <careerforma@gmail.com>
2021-12-01 19:45:05 +0300

    from wildcard to netmask

--- topologies/medium_topology/theNetworkBuilderScript.py
+++ topologies/medium_topology/theNetworkBuilderScript.py
@@ -44,7 +44,7 @@ def addRoutersInterfaces(net:Mininet, data:dict):
     for r in routers:
         device=net.getNodeByName(r)
         for interface,address in getAllInterfacesaAndIPsOfRouters(data,r,True).items():
-            device.cmd(f"ifconfig {interface} {ipaddress.ip_network(address, strict=False).netmask} ")
+            device.cmd(f"ifconfig {interface} {removeWildCard(address)} netmask {ipaddress.ip_network(address, strict=False).netmask} ")
             info("added interface: " + interface + address + "\n")
     
 
