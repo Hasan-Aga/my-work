a0408f5288101dcac445f804f6550fcd4a0eedbe
Hasan-Alsulaiman <careerforma@gmail.com>
2021-12-01 19:59:04 +0300

    log before command

--- topologies/medium_topology/theNetworkBuilderScript.py
+++ topologies/medium_topology/theNetworkBuilderScript.py
@@ -44,8 +44,8 @@ def addRoutersInterfaces(net:Mininet, data:dict):
     for r in routers:
         device=net.getNodeByName(r)
         for interface,address in getAllInterfacesaAndIPsOfRouters(data,r,True).items():
-            device.cmd(f"ifconfig {interface} {removeWildCard(address.split('-')[1])} netmask {ipaddress.ip_network(address, strict=False).netmask} up ")
             info("ifconfig " + interface + " " + removeWildCard(address.split('-')[1]) + " netmask " + str(ipaddress.ip_network(address, strict=False).netmask) + "\n")
+            device.cmd(f"ifconfig {interface} {removeWildCard(address.split('-')[1])} netmask {ipaddress.ip_network(address, strict=False).netmask} up ")
     
 
 
