1d31111dcee61877fd4e059814dccb4aa9f2861a
None <None>
2021-12-29 09:53:14 -0800

    define build and switches

--- topologies/onlySwitches/buildTopology.py
+++ topologies/onlySwitches/buildTopology.py
@@ -6,3 +6,9 @@ from mininet.cli import CLI
 
 class SwitchTopo(Topo):
     "topology using only switches for routing"
+    def build( self, **_opts ):
+        
+        s1 = self.addSwitch("s1", cls=OVSSwitch, protocols="OpenFlow13")
+        s2 = self.addSwitch("s2", cls=OVSSwitch, protocols="OpenFlow13")
+        s3 = self.addSwitch("s3", cls=OVSSwitch, protocols="OpenFlow13")
+        s4 = self.addSwitch("s4", cls=OVSSwitch, protocols="OpenFlow13")
