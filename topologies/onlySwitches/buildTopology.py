afdd90ce5a9b0ea7595016ef57309680c433ed96
None <None>
2021-12-29 10:00:17 -0800

    fix descrription of topo

--- topologies/onlySwitches/buildTopology.py
+++ topologies/onlySwitches/buildTopology.py
@@ -5,7 +5,7 @@ from mininet.log import setLogLevel, info
 from mininet.cli import CLI
 
 class SwitchTopo(Topo):
-    "topology using only switches for routing"
+    # A topology using only switches for routing
     def build( self, **_opts ):
         
         # Add switches
@@ -25,3 +25,5 @@ class SwitchTopo(Topo):
         self.addLink(rightHost, s3)
         self.addLink(s3, s4)
         self.addLink(s4, s1)
+
+topos = { 'mytopo': ( lambda: SwitchTopo() ) }
