58af8c75749603170f6264a1d3e4f31d0fe170c7
None <None>
2021-12-29 09:58:39 -0800

    add links

--- topologies/onlySwitches/buildTopology.py
+++ topologies/onlySwitches/buildTopology.py
@@ -8,7 +8,20 @@ class SwitchTopo(Topo):
     "topology using only switches for routing"
     def build( self, **_opts ):
         
+        # Add switches
         s1 = self.addSwitch("s1", cls=OVSSwitch, protocols="OpenFlow13")
         s2 = self.addSwitch("s2", cls=OVSSwitch, protocols="OpenFlow13")
         s3 = self.addSwitch("s3", cls=OVSSwitch, protocols="OpenFlow13")
         s4 = self.addSwitch("s4", cls=OVSSwitch, protocols="OpenFlow13")
+
+        # Add hosts 
+        leftHost = self.addHost( 'h1' )
+        rightHost = self.addHost( 'h2' )
+
+        # Add links
+        self.addLink(leftHost, s1)
+        self.addLink(s1, s2)
+        self.addLink(s2, s3)
+        self.addLink(rightHost, s3)
+        self.addLink(s3, s4)
+        self.addLink(s4, s1)
