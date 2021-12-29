5f4195a51ebd4c962d15dd177decc6f02a83e7b0
None <None>
2021-12-29 10:00:50 -0800

    name the topology

--- topologies/onlySwitches/buildTopology.py
+++ topologies/onlySwitches/buildTopology.py
@@ -26,4 +26,4 @@ class SwitchTopo(Topo):
         self.addLink(s3, s4)
         self.addLink(s4, s1)
 
-topos = { 'mytopo': ( lambda: SwitchTopo() ) }
+topos = { 'switchTopo': ( lambda: SwitchTopo() ) }
